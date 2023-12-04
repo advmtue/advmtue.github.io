b83ed621a6a72ce5d2b901d24dfba73d56644d6f
Adam Tuechler <adamtuechler@gmail.com>
2023-12-04 18:51:11 +1100

    First e2e

--- main.mjs
+++ main.mjs
@@ -2,23 +2,29 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import yaml from 'yaml'
 import { marked } from 'marked'
+import pug from 'pug'
 
 const FRONTMATTER_SPLITTER = '---\n'
+const CONTENT_DIR = 'content'
+const OUTPUT_DIR = 'build'
 
-const extractFrontMatter = rawContent => {
-    return rawContent.split(FRONTMATTER_SPLITTER)[1]
-}
+const compilePage = pug.compileFile(path.join('views', 'template.pug'))
 
-const extractBody = rawContent => {
-    return rawContent.split(FRONTMATTER_SPLITTER).slice(2).join(FRONTMATTER_SPLITTER)
-}
+// Delete and recreate the output dir
+await fs.rm(OUTPUT_DIR, { force: true, recursive: true })
+await fs.mkdir(OUTPUT_DIR)
+await fs.mkdir(path.join(OUTPUT_DIR, CONTENT_DIR))
 
 const filenames = await fs.readdir('content')
 
 for (const filename of filenames) {
-    const rawContent = await fs.readFile(path.join('content', filename), 'utf-8')
-    const rawFrontMatter = extractFrontMatter(rawContent)
-    const rawBody = extractBody(rawContent)
+    const rawContent = await fs.readFile(path.join(CONTENT_DIR, filename), 'utf-8')
+    const rawFrontMatter = rawContent.split(FRONTMATTER_SPLITTER)[1]
+    const rawBody = rawContent.split(FRONTMATTER_SPLITTER).slice(2).join(FRONTMATTER_SPLITTER)
     const frontMatter = yaml.parse(rawFrontMatter)
     const contentHTML = marked.parse(rawBody)
+    const htmlPage = compilePage({ contentHTML, frontMatter })
+    const htmlFileName = `${filename.split('.').slice(0, -1).join('.')}.html`
+
+    await fs.writeFile(path.join(OUTPUT_DIR, CONTENT_DIR, htmlFileName), htmlPage)
 }
