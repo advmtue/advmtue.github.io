b52c80646b274e4a99ebf0c336c70cd234825b51
Adam Tuechler <adamtuechler@gmail.com>
2023-12-22 20:25:39 +1100

    Add index page

--- main.mjs
+++ main.mjs
@@ -9,6 +9,7 @@ const CONTENT_DIR = 'content'
 const OUTPUT_DIR = 'build'
 
 const compilePage = pug.compileFile(path.join('views', 'template.pug'))
+const compileIndex = pug.compileFile(path.join('views', 'index.pug'))
 
 // Delete and recreate the output dir
 await fs.rm(OUTPUT_DIR, { force: true, recursive: true })
@@ -16,6 +17,7 @@ await fs.mkdir(OUTPUT_DIR)
 await fs.mkdir(path.join(OUTPUT_DIR, CONTENT_DIR))
 
 const blogFilenames = await fs.readdir('content')
+const pages = []
 
 for (const filename of blogFilenames) {
     const rawContent = await fs.readFile(path.join(CONTENT_DIR, filename), 'utf-8')
@@ -25,9 +27,16 @@ for (const filename of blogFilenames) {
     const contentHTML = marked.parse(rawBody)
     const htmlPage = compilePage({ contentHTML, frontMatter })
     const htmlFileName = `${filename.split('.').slice(0, -1).join('.')}.html`
+    const contentPath = path.join(CONTENT_DIR, htmlFileName)
 
-    await fs.writeFile(path.join(OUTPUT_DIR, CONTENT_DIR, htmlFileName), htmlPage)
+    await fs.writeFile(path.join(OUTPUT_DIR, contentPath), htmlPage)
+
+    pages.push({ contentPath, ...frontMatter })
 }
+
+const sortedPages = pages.sort((a, b) => a.datetime < b.datetime ? -1 : 1)
+const htmlIndex = compileIndex({ sortedPages })
+await fs.writeFile(path.join(OUTPUT_DIR, 'index.html'), htmlIndex)
 
 // Copy static assets
 await fs.cp('static', 'build', { recursive: true })
